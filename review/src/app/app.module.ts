14ce910c770fa7e84b6d31e0508b3218db9da0ed
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-20 20:55:59 -0600

    completed second exercise.. needed a tiny help with last problem

--- review/src/app/app.module.ts
+++ review/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,9 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- review/src/app/success-alert/success-alert.component.ts
+++ review/src/app/success-alert/success-alert.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
   styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent {
+  username:string=""
+
+
+
+
+
+
+
+
+
+  reset(){
+    this.username=""
+
+  }
+
 
 }
