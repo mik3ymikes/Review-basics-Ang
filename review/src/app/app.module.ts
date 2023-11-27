b0006f114c897ae08d2581f189554d37ab2f6240
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-27 15:05:15 -0600

    setting up routing

--- review/src/app/app.module.ts
+++ review/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
@@ -22,7 +22,7 @@ import { ServExComponent } from './serv-ex/serv-ex.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
     FormsModule
 
   ],
